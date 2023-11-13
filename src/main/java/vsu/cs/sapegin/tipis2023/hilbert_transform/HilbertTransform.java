c1633ddd02fa22068a4003fce780135038b5c20c
Pavel Sapegin <pavelsapegin18@gmail.com>
2023-11-13 20:20:26 +0300

    Final version

--- src/main/java/vsu/cs/sapegin/tipis2023/hilbert_transform/HilbertTransform.java
+++ src/main/java/vsu/cs/sapegin/tipis2023/hilbert_transform/HilbertTransform.java
@@ -4,31 +4,32 @@ import vsu.cs.sapegin.tipis2023.dft.Complex;
 import vsu.cs.sapegin.tipis2023.dft.DFT;
 
 public class HilbertTransform {
+    //Физический смысл: фазовый сдвиг всех спектральных составляющих исходного сигнала на pi/2
+    //множество мгновенных амплитуд - огибающая
     public static Complex[] hilbertTransform(Complex[] signal) {
         int length = signal.length;
 
-        // Выполнение дискретного преобразования Фурье (DFT)
+        //Выполняем fft
         Complex[] spectrum = DFT.fft(signal);
 
-        // Создание фильтра Гильберта
+        //Создаём фильтр Гильберта
         Complex[] hilbertFilter = new Complex[length];
         int mid = length / 2;
         hilbertFilter[0] = new Complex();
         hilbertFilter[mid] = new Complex(1, 1);
         for (int i = 1; i < mid; i++) {
-            double sign = (i % 2 == 0) ? -1.0 : 1.0; //по сути, меняет знаки
+            double sign = (i % 2 == 0) ? -1.0 : 1.0;
             hilbertFilter[i] = new Complex(sign * 2, sign * 2);
             hilbertFilter[length - i] = new Complex(sign * 2, sign * 2);
         }
 
-        // Умножение спектра на фильтр Гильберта
+        //Умножаем спектр на фильтр Гильберта
         Complex[] transformedSpectrum = new Complex[length];
         for (int i = 0; i < length; i++) {
             transformedSpectrum[i] = spectrum[i].multiply(hilbertFilter[i]);
         }
-        // Выполнение инверсного дискретного преобразования Фурье (IDFT)
-        Complex[] transformedSignal = DFT.ifft(transformedSpectrum);
 
-        return transformedSignal;
+        //Выполняем ifft
+        return DFT.ifft(transformedSpectrum);
     }
 }
